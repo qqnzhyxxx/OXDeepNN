ac74e24009a821d694b2d6ed7abd2c5ac7881b01
qqnzhyxxx <tbnzhyxxx@163.com>
2017-04-21 23:51:51 +0800

    Add MLP Demo

---
+++ demo/MLPerceptron/MLPerceptron.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <memory>
+#include "QTplot/qcustomplot.h"
+#include "layers/NeureLayer.h"
+#include "Enumeration.h"
+#include <QApplication>
+using namespace std;
+/*************************************************
+Demo intruduce: Show the Artificial Neure work condition
+                in MLPerceptron
+Author: Niu ZhiYong
+Date: 2017-04-20
+tips:
+**************************************************/
+int main(int argc, char *argv[])
+{
+    cout<<"MLPerceptron Test"<<endl;
+    shared_ptr< NeureLayer > layer = make_shared<NeureLayer>(NeureLayer(1,SigmoidEnum,2,true));
+
+    int i = (layer->GetNeureList()).at(0)->GetInputNum();
+    cout<<"m_InputNum = "<<i<<endl;
+}
